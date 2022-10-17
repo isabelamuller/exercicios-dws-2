84a39d92d4a021342929ebd5f0ce93ef36daa7b9
Isabela Müller <isabela.muller-ext@dentsu.com>
2022-10-17 11:12:52 -0300

    axios test

---
+++ axios/main.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+class Api{
+
+    static async getAddress(cep) {
+        const response = new Address((await axios.get(`https://viacep.com.br/ws/${cep}/json/`)).data);
+        return response;
+        
+    }
+}
+    class Address{
+        constructor({logradouro, bairro, localidade}){
+            this.logradouro = logradouro,
+            this.bairro = bairro,
+            this.localidade = localidade
+        }
+    }
+Api.getAddress('93336010').then(v => {console.log(v)})
